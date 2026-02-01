ec3d7572b2218920a60199337b6205f92bbf5444
Henry Brown <henry.brown321@gmail.com>
2026-02-01 21:52:46 +0000

    Add YAML job configuration file reader using Jackson

---
+++ src/main/java/com/github/henrybrown123/JobConfigFileReader.java
@@ -0,0 +1,22 @@
+package com.github.henrybrown123;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+public class JobConfigFileReader {
+    // record type to allow deserialization into a List of jobs without needing to
+    record JobConfigFile(List<JobData> jobs) {}
+
+    public static List<JobData> read(Path file) throws IOException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        mapper.registerModule(new JavaTimeModule());
+        JobConfigFile jobsFile = mapper.readValue(file.toFile(), JobConfigFile.class);
+        return jobsFile.jobs();
+    }
+
+}
