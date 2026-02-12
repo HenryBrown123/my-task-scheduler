7676a9122518673633c464e0b5bfd2dd9261b677
Henry Brown <henry.brown321@gmail.com>
2026-02-12 22:49:56 +0000

    refactor: add InvalidConfigException and RepositoryException for cleaner error handling

--- src/main/java/com/github/henrybrown123/configuration/InvalidConfigException.java
+++ src/main/java/com/github/henrybrown123/configuration/InvalidConfigException.java
@@ -1,7 +1,44 @@
 package com.github.henrybrown123.configuration;
 
-public class InvalidConfigException extends RuntimeException {
-  public InvalidConfigException(String message) {
-    super(message);
-  }
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
+
+import java.nio.file.Path;
+
+/**
+ * This is a bespoke Exception that wraps some common Jackson de-serialization exceptions to form more user-friendly
+ * error messages. Like when a mandatory property is missing or the wrong data type is used... (pretty common)
+ *
+ */
+public class InvalidConfigException extends Exception {
+
+    public InvalidConfigException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+
+    /**
+     * Specific constructor for jackson databind exceptions to allow human-readable error messages to be output.
+     * @param configPath
+     * @param e
+     */
+    public InvalidConfigException(Path configPath, DatabindException e) {
+        super(buildMessage(configPath, e), e);
+    }
+
+    private static String buildMessage(Path configPath, DatabindException e) {
+        String detail = switch (e) {
+            case UnrecognizedPropertyException u ->
+                    "Unknown property '" + u.getPropertyName() + "' at " + u.getLocation();
+            case InvalidFormatException f ->
+                    "Invalid value '" + f.getValue() + "' at " + f.getLocation();
+            case MismatchedInputException m ->
+                    "Wrong type for field at " + m.getLocation() + ": " + m.getOriginalMessage();
+            default ->
+                    e.getOriginalMessage();
+        };
+
+        return "Invalid config in " + configPath + ": " + detail;
+    }
 }
--- src/main/java/com/github/henrybrown123/repository/RepositoryException.java
+++ src/main/java/com/github/henrybrown123/repository/RepositoryException.java
@@ -1,7 +1,8 @@
 package com.github.henrybrown123.repository;
 
+
 public class RepositoryException extends RuntimeException {
-  public RepositoryException(String message) {
-    super(message);
-  }
+    public RepositoryException(String message, Exception e) {
+        super(message, e);
+    }
 }
